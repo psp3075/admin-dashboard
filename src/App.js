98687c2632e9e88e1cfd1c19bc7861a078a386d6
Pavan S Patil <psp3075@gmail.com>
2023-01-05 10:40:26 +0530

    setup test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      React
-    </div>
-  );
+  return <div className="text-3xl">React</div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
